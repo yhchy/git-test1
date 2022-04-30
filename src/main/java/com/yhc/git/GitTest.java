993fa2dcfdd39270208ac737288c7eb2a488a893
yhc <yhc@yhc.com>
2022-04-30 09:36:38 +0800

    first commit

---
+++ src/main/java/com/yhc/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yhc.git;
+
+/**
+ * @Author: Yang Hengcan
+ * @Date: 2022/04/30 9:32
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
